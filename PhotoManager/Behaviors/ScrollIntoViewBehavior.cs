60f328ba9194ca8700633c0cd9c8019ee28ca061
SchreinerK <ks@ksware.de>
2019-07-21 19:16:18 +0200

    ScrollIntoViewBehavior

---
+++ PhotoManager/Behaviors/ScrollIntoViewBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace KsWare.PhotoManager.Behaviors
+{
+	public class ScrollIntoViewBehavior : Behavior<ListBox>
+	{
+		protected override void OnAttached()
+		{
+			base.OnAttached();
+			this.AssociatedObject.SelectionChanged += AssociatedObject_SelectionChanged;
+		}
+
+		void AssociatedObject_SelectionChanged(object sender,
+			SelectionChangedEventArgs e)
+		{
+			if (sender is ListBox)
+			{
+				ListBox listBox = (sender as ListBox);
+				if (listBox.SelectedItem != null)
+				{
+					listBox.Dispatcher.BeginInvoke(
+						(Action)(() =>
+						{
+							listBox.UpdateLayout();
+							if (listBox.SelectedItem !=
+							    null)
+								listBox.ScrollIntoView(
+									listBox.SelectedItem);
+						}));
+				}
+			}
+		}
+
+		protected override void OnDetaching()
+		{
+			base.OnDetaching();
+			this.AssociatedObject.SelectionChanged -=
+				AssociatedObject_SelectionChanged;
+
+		}
+	}
+}
